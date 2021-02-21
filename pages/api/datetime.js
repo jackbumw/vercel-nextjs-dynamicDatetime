55fce3d4b3b42452338dd9f2af4e7822fdf74521
jackbumw <jack.bu@meltwater.com>
2021-02-21 13:26:58 +0800

    Create datetime.js

---
+++ pages/api/datetime.js
@@ -0,0 +1,22 @@
+const fetch = require("node-fetch");
+
+const fetchCharacters = async () => {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const { results } = await res.json();
+
+  // ADD ONE HERE
+  console.log(results);
+  return results;
+};
+
+export default async (req, res) => {
+  try {
+    const characters = await fetchCharacters();
+
+    // ADD ONE HERE
+    console.log(characters)
+    res.status(200).json({ characters });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
