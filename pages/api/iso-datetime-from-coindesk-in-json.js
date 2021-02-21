78477219d21fe947a9d3b54c1a959a65e22c4240
jackbumw <jack.bu@meltwater.com>
2021-02-21 15:38:07 +0800

    xml

---
+++ pages/api/iso-datetime-from-coindesk-in-json.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch");
+
+const fetchCharacters = async () => {
+  const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+  const results = await res.json();
+  // ADD ONE HERE
+  console.log(results);
+  return results;
+};
+
+export default async (req, res) => {
+  try {
+    const characters = await fetchCharacters();
+    // ADD ONE HERE
+    console.log(characters)
+    res.status(200).json({ time: characters.time.updatedISO });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+};
--- pages/api/iso-datetime-from-coindesk-in-xml.js
+++ pages/api/iso-datetime-from-coindesk-in-xml.js
@@ -3,20 +3,17 @@ const fetch = require("node-fetch");
 const fetchCharacters = async () => {
   const res = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
   const results = await res.json();
-
   // ADD ONE HERE
-  console.log(results);
   return results;
 };
 
 export default async (req, res) => {
   try {
     const characters = await fetchCharacters();
-
     // ADD ONE HERE
-    console.log(characters)
-    res.status(200).json({ characters });
+    console.log(characters.time.updatedISO);
+    res.send(`<?xml version="1.0" encoding="utf-8"?><datetime>${characters.time.updatedISO}</datetime>`);
   } catch (error) {
-    res.status(500).json({ error });
+    res.status(500).json({ error: error.message });
   }
 };
